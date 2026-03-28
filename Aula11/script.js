8fbac64127c8a73f4cd40cd38583526b1cc3303e
Walmo Santos <walmo.santos09@aluno.ifce.edu.br>
2026-03-27 21:51:38 -0300

    Aula 11 Concluida

---
+++ Aula11/script.js
@@ -0,0 +1,28 @@
+// alert('Ola mundo2')
+
+// let nome = 'Walmo'
+// let sobrenome = 'Santos'
+// var idade = 11
+// const altura = 1.75
+
+// console.log(nome)
+// console.log(idade)
+// console.log(altura)
+// console.log("==========================================================================================")
+// nome = 'Walmo'
+// idade = 19
+// const altura2 = 1.7011
+
+// console.log(`Nome: ${nome} Sobrenome: ${sobrenome}`)
+// console.log(idade)
+// console.log(altura2)
+
+// let nome = prompt('Insira seu nome')
+// let idade = prompt('Insira sua idade')
+// console.log(`Olá ${nome}, seja bem-vindo!!, sua idade é ${idade} anos`)
+
+// let numero1 = prompt('Escreva o numero 1')
+// let numero2 = prompt('Escreva o numero 2') 
+// let soma = numero1 + numero2
+// console.log(soma)
+
