df635cb4b233716b1b15e64266ad0eed85db801c
Walmo <walmosantos09@aluno.ifce.edu.br>
2026-04-01 11:09:44 -0300

    Concluida e corrigida questão 4

--- Aula12/questão4.js
+++ Aula12/questão4.js
@@ -1,9 +1,16 @@
-let ponto = prompt('digite o horario')
+let ponto = prompt('digite o horario (HH:MM)')
 
-let p = horario.indexOf(":");
-let hr = Number(horario.slice(0, p));
-let min = number(horario.slice(pos + 1));
+let p = ponto.indexOf(":");
+let hr = Number(ponto.slice(0, p));
+let min = Number(ponto.slice(p + 1));
 
+let digitadoEmMinutos = hr * 60 + min;
+let limiteEmMinutos = 21 * 60 + 30;
 
+if (digitadoEmMinutos < limiteEmMinutos) {
+    console.log("Hanna vai brigar com Vc!!!!")
+} else {
+    alert("vc esta a salvo da hanna (Por enquanto)")
+}
 
 
