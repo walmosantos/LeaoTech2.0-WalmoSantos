65b3a4cfb41d3d6ac6e09de461b4b224a9143d42
Walmo Santos do Nascimento <walmo.santos09@aluno.ifce.edu.br>
2026-06-05 21:07:14 -0300

    aula 34

---
+++ Aula34/app.js
@@ -0,0 +1,109 @@
+const prompt = require('prompt-sync') ()
+const { createClient } = require('@supabase/supabase-js')
+require('dotenv').config()
+
+const supabase = createClient (
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+)
+
+async function inserirAutor() {
+    let nome = prompt('Digite o nome do autor: ')
+    let nacionalidade = prompt("Digite a nacionalidade: ")
+
+    let novoAutor = {
+        nome:nome, //direita esta no banco e esquerda na função
+        nacionalidade:nacionalidade
+    }
+    const {data, error} = await supabase.from('biblioteca_autor').insert(novoAutor).select()
+    console.log(data)
+    console.log(error)
+}
+//inserirAutor()
+
+async function inserirLivro() {
+    let titulo = prompt('Digite o none do livro: ')
+    let quantidade = parseInt(prompt('Insira a quantidade de livros: '))
+    let genero = prompt('Insira o genero: ')
+    let id_autor = parseInt(prompt('insira o id do livro: '))
+
+    let novoLivro = {
+        titulo:titulo,
+        quantidade:quantidade,
+        genero:genero,
+        id_autor:id_autor
+    }
+     const {data, error} = await supabase.from('biblioteca_livro').insert(novoLivro).select()
+    console.log(data)
+    console.log(error)
+
+}
+//inserirLivro()
+
+async function inserirUser() {
+    let nome = prompt('Insira seu nome: ')
+    let cpf = prompt('Insira seu cpf: ')
+    let telefone = prompt('Insira seu telefone: ')
+    let endereco = prompt('Insira seu endereço: ')
+
+    let novoUser = {
+        nome:nome,
+        cpf:cpf,
+        telefone:telefone,
+        endereco:endereco
+    }
+
+      const {data, error} = await supabase.from('biblioteca_usuarios').insert(novoUser).select()
+    console.log(data)
+    console.log(error)
+}
+
+//inserirUser()
+
+async function inserirPerfil() {
+    let foto = prompt('Insira sua foto:')
+    let bio = prompt('Insira sua bio: ')
+    let id_usuario = (prompt('insira o id: '))
+
+    let novoPerfil = {
+        foto:foto,
+        bio:bio,
+        id_usuario
+    }
+
+      const {data, error} = await supabase.from('biblioteca_perfil').insert(novoPerfil).select()
+    console.log(data)
+    console.log(error)
+}
+
+//inserirPerfil()
+
+async function inserirEmprestimo() {
+    let id_livro = prompt('Insira o ID do livro desejado:')
+    let id_usuario = prompt('Insira seu ID: ')
+    let data_entrega = prompt('Insira a data de entrega: ')
+    let data_devolucao = prompt('Insira a data de devolução: ')
+
+    let novoEmprestimo = {
+        id_livro:id_livro,
+        id_usuario:id_usuario,
+        data_entrega:data_entrega,
+        data_devolucao:data_devolucao
+    }
+
+      const {data, error} = await supabase.from('biblioteca_emprestimo').insert(novoEmprestimo).select()
+    console.log(data)
+    console.log(error)
+}
+//inserirEmprestimo()
+
+async function listarLivros() {
+    const {data, error} = await supabase.from("biblioteca_livros").select('*')
+    console.log(data)
+    console.log(error)
+
+    data.forEach(livro => {
+        console.log(`Titulo: ${livro.titulo}, Gênero: ${livro.genero}`);
+        
+    })
+}
